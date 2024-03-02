753466aef6350629f7a578412cf0dae24a2dee05
Katie K <katiekucsera@gmail.com>
2024-03-02 14:54:54 -0500

    Fix update dog action

--- src/features/dogSlice.js
+++ src/features/dogSlice.js
@@ -24,24 +24,12 @@ export const dogSlice = createSlice({
       return state.dogs.find(dog => dog.id === action.payload);
     },
     updateDog: (state, action) => {
-      const dog = state.dogs.findIndex(dog => dog.id === action.payload.id);
+      const index = state.dogs.findIndex(dog => dog.id === action.payload.id);
       if (index !== -1 && action.payload) {
-        dog.name = action.payload.name;
-        dog.age = action.payload.age;
-        dog.sex = action.payload.sex;
-        dog.breed = action.payload.breed;
-        dog.size = action.payload.size;
-        dog.description = action.payload.description;
-        dog.status = action.payload.status;
-        dog.energy_level = action.payload.energy_level;
-        dog.social_media_link = action.payload.social_media_link;
-        dog.good_with_dogs = action.payload.good_with_dogs;
-        dog.good_with_cats = action.payload.good_with_cats;
-        dog.good_with_kids = action.payload.good_with_kids;
-        dog.foster_location = action.payload.foster_location;
-        dog.medical_conditions = action.payload.medical_conditions;
-        dog.adoption_fee = action.payload.adoption_fee;
-        dog.default_image = action.payload.default_image;
+        state.dogs[index] = {
+          ...state.dogs[index],
+          ...action.payload
+        };
       }
     },
     deleteDog: (state, action) => {
@@ -51,18 +39,19 @@ export const dogSlice = createSlice({
       }
     }
   },
-  extraReducers: {
-    [fetchDogs.pending]: (state) => {
-      state.status = 'loading';
-    },
-    [fetchDogs.fulfilled]: (state, action) => {
-      state.status = 'succeeded';
-      state.dogs = state.dogs.concat(action.payload);
-    },
-    [fetchDogs.rejected]: (state, action) => {
-      state.status = 'failed';
-      state.error = action.payload;
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchDogs.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchDogs.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.dogs = state.dogs.concat(action.payload);
+      })
+      .addCase(fetchDogs.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.payload;
+      });
   },
 });
 
