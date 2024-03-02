8cb44891811bf078d5d2b44926508930c27f50e8
Katie K <katiekucsera@gmail.com>
2024-03-02 14:55:15 -0500

    Import dogs reducer

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/userSlice';
+import dogsReducer from './features/dogSlice';
 
 export const store = configureStore({
   reducer: {
