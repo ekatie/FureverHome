68fd4a1aa61f320940bfa6a055b78d33026f3000
Katie K <katiekucsera@gmail.com>
2024-03-02 14:18:38 -0500

    Base api and dog functions

---
+++ src/services/dogsService.js
@@ -0,0 +1,46 @@
+import API from './api';
+
+export const getDogs = async () => {
+  try {
+    const response = await API.get('/dogs');
+    return response.data;
+  } catch (error) {
+   throw error;
+  }
+}
+
+export const getDog = async (id) => {
+  try {
+    const response = await API.get(`/dogs/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export const addDog = async (dog) => {
+  try {
+    const response = await API.post('/dogs', dog);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export const updateDog = async (id, dog) => {
+  try {
+    const response = await API.put(`/dogs/${id}`, dog);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export const deleteDog = async (id) => {
+  try {
+    const response = await API.delete(`/dogs/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
