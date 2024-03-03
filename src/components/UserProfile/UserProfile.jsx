1e4d79287a0b682d23feaa5102de6cd28c4a9778
Katie K <katiekucsera@gmail.com>
2024-03-03 17:13:06 -0500

    Add user signed in check to profile

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function UserProfile() {
+  const user = useSelector((state) => state.user);
+
+  if (!user) {
+    return <h1>Not logged in</h1>;
+  }
+
   return (
     <div>
       <h1>User Profile</h1>
