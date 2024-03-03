657215c51964ca91948a6929c5eecf33a9f05fc6
Katie K <katiekucsera@gmail.com>
2024-03-03 17:13:36 -0500

    Refactor user auth slice

--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -1,20 +1,27 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  user: null,
+  token: null
+};
+
 // login, logout and register are all actions, api calls are thunks
 const authSlice = createSlice({
   name: "auth",
-  initialState: {
-    user: {},
-  },
+  initialState,
   reducers: {
     login(state, action) {
-      state.user = action.payload;
+      state.user = action.payload.user;
+      state.token = action.payload.token;
     },
     logout(state) {
-      state.user = {};
+      state.user = null;
+      state.token = null;
+      localStorage.removeItem('jwt');
     },
     register(state, action) {
-      state.user = action.payload;
+      state.user = action.payload.user;
+      state.token = action.payload.token;
     },
   },
 });
