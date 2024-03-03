8d79db19750342ab98e4e879d98d7b9b97f5df1d
Katie K <katiekucsera@gmail.com>
2024-03-03 14:22:09 -0500

    Reset component for use with redux

--- src/components/DogList/DogList.jsx
+++ src/components/DogList/DogList.jsx
@@ -1,26 +1,9 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { fetchDogs } from "../../features/dogSlice";
+import React from "react";
 
 const DogsList = () => {
-  const dispatch = useDispatch();
-  const { dogs, status, error } = useSelector((state) => state.dogs);
-
-  useEffect(() => {
-    dispatch(fetchDogs());
-  }, [dispatch]);
-
-  if (status === "loading") {
-    return <div>Loading...</div>;
-  }
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
   return (
     <div>
-      <h1>Dog Details</h1>
+      <h1>Dog List</h1>
       {/* {dogs.map((dog) => (
         <div key={dog.id}>
           <Link to={`/dogs/${dog.id}`}>
