2192e4efc69540bf901384d62878f8f5f3accba2
Katie K <katiekucsera@gmail.com>
2024-03-03 14:23:54 -0500

    Create skeletor for login and user auth

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { login } from "../../features/authSlice";
 
 function Login() {
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
   return (
     <div>
       <h1>Login</h1>
+      <form>
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit" onClick={dispatch(login())}>
+          Login
+        </button>
+      </form>
     </div>
   );
 }
