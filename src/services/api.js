fab25b1ccd3df6f9737bcf849693b6295946c110
Katie K <katiekucsera@gmail.com>
2024-03-04 18:50:03 -0500

    Update base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "http://localhost:3001/api/v1",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json",
