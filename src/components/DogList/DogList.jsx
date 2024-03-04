e02078e5e55fcfb3114383a6c60dd2e03947cb6d
Katie K <katiekucsera@gmail.com>
2024-03-04 18:49:02 -0500

    Implement dog list component to render dog images and link to details

--- src/components/DogList/DogList.jsx
+++ src/components/DogList/DogList.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchDogsAsync } from "../../features/dogSlice";
+import { Link } from "react-router-dom";
+import DogImage from "../DogImage/DogImage";
 
 const DogsList = () => {
+  const dispatch = useDispatch();
+  const dogs = useSelector((state) => state.dogs.dogs);
+  const status = useSelector((state) => state.dogs.status);
+
+  useEffect(() => {
+    dispatch(fetchDogsAsync());
+  }, [dispatch]);
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+  if (status === "failed") {
+    return <div>Failed to load dogs</div>;
+  }
+
   return (
     <div>
-      <h1>Dog List</h1>
-      {/* {dogs.map((dog) => (
-        <div key={dog.id}>
-          <Link to={`/dogs/${dog.id}`}>
-            <h3>{dog.name}</h3>
-          </Link>
-          <p>{dog.default_image}</p>
-        </div>
-      ))} */}
+      {dogs.map((dog) => (
+        <Link to={`/dogs/${dog.id}`}>
+          <DogImage
+            key={dog.id}
+            name={dog.name}
+            imageUrl={dog.defaultImageUrl}
+          />
+        </Link>
+      ))}
     </div>
   );
 };
