463adc3222faca583b674873fa8c863b7c67579b
Katie K <katiekucsera@gmail.com>
2024-03-05 17:37:24 -0500

    Implement dog image component and styling

--- src/components/DogImage/DogImage.jsx
+++ src/components/DogImage/DogImage.jsx
@@ -1,15 +1,9 @@
 import React from "react";
+import "./DogImage.scss";
 
 const DogImage = ({ name, imageUrl }) => {
   return (
-    <div>
-      <h2>{name}</h2>
-      <img
-        src={imageUrl}
-        alt={name}
-        style={{ width: "100%", height: "auto" }}
-      />
-    </div>
+      <img  className="dog-image" src={imageUrl} alt={name} />
   );
 };
 
