3a7aae04d0898a7244d417505947aae382ac2acf
Katie K <katiekucsera@gmail.com>
2024-03-07 19:02:57 -0500

    Update store config

--- src/store.js
+++ src/store.js
@@ -1,16 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
+
 import dogsReducer from "./features/dogSlice";
 import userReducer from "./features/userSlice";
 import applicationReducer from "./features/applicationSlice";
 import authReducer from "./features/authSlice";
 
-const store = configureStore({
-  reducer: {
-    dogs: dogsReducer,
-    user: userReducer,
-    application: applicationReducer,
-    auth: authReducer,
-  },
+const rootReducer = combineReducers({
+  dogs: dogsReducer,
+  user: userReducer,
+  application: applicationReducer,
+  auth: authReducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
 });
 
 export default store;
