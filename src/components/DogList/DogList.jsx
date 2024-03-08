a074cfdc7f549eeecddd54cf6f627ffeadc8dde2
Katie K <katiekucsera@gmail.com>
2024-03-08 18:57:17 -0500

    Refactor dog list component and styling

--- src/components/DogList/DogList.jsx
+++ src/components/DogList/DogList.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchDogsAsync } from "../../features/dogSlice";
-import { Link } from "react-router-dom";
-import DogImage from "../DogImage/DogImage";
+import { fetchDogsAsync, toggleFavouriteAsync } from "../../features/dogSlice";
+import DogListItem from "../DogListItem/DogListItem";
 import "./DogList.scss";
 
 const DogList = () => {
@@ -23,24 +22,39 @@ const DogList = () => {
 
   return (
     <div>
-      {/* filter dogs by size, age */}
       <h1 className="page-title">Available Dogs</h1>
-      <section className="dog-list">
+      <ul className="dog-list">
         {dogs.map((dog) => (
-          <article key={dog.id} className="dog-list-item">
-            <Link to={`/dogs/${dog.id}`} className="dog-link">
-              {dog.name}
-            </Link>
-            <DogImage
-              key={dog.id}
-              name={dog.name}
-              imageUrl={dog.default_image_url}
-            />
-          </article>
+          <DogListItem
+            key={dog.id}
+            name={dog.name}
+            imageUrl={dog.default_image_url}
+            isFavourite={dog.isFavourite}
+            onToggleFavourite={() =>
+              dispatch(toggleFavouriteAsync({ dogId: dog.id }))
+            }
+          />
         ))}
-      </section>
+      </ul>
     </div>
   );
 };
 
 export default DogList;
+
+// <article key={dog.id} className="dog-list-item">
+//   <Link to={`/dogs/${dog.id}`} className="dog-link">
+//     {dog.name}
+//   </Link>
+//   <DogListItem
+//     key={dog.id}
+//     name={dog.name}
+//     imageUrl={dog.default_image_url}
+//   />
+//   <FavButton
+//     selected={dog.isFavourite}
+//     onToggleFavourite={() =>
+//       dispatch(toggleFavouriteAsync({ dogId: dog.id }))
+//     }
+//   />
+// </article>
