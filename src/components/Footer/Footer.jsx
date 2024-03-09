833d3e55037c1ae33243057da579d49047cb3d9d
Katie K <katiekucsera@gmail.com>
2024-03-09 18:18:00 -0500

    Implement footer component and styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,35 @@
 import React from "react";
+import "./Footer.scss";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import XIcon from "@mui/icons-material/X";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 
 function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="footer">
+      <p>Furever Home</p>
+      <div className="social-media">
+        <a href="https://www.instagram.com/" className="social-link">
+          <InstagramIcon />
+        </a>
+        <a href="https://www.facebook.com/" className="social-link">
+          <FacebookIcon />
+        </a>
+        <a href="https://twitter.com" className="social-link">
+          <XIcon />
+        </a>
+        <a href="https://www.youtube.com/" className="social-link">
+          <YouTubeIcon />
+        </a>
+      </div>
+      <a href="mailto:info@fureverhome.com" className="email-link">
+        info@fureverhome.com
+      </a>
+    </footer>
   );
 }
 
 export default Footer;
+
+// set up email text to link to send mail to
