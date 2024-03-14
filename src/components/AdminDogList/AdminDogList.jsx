b2c9079119243a320511afe2e314218c3d7d20f6
Katie K <katiekucsera@gmail.com>
2024-03-14 17:55:08 -0400

    Initiate admin dog list component

---
+++ src/components/AdminDogList/AdminDogList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminDogList = () => {
+  return (
+    <section className="admin-dog-list">
+      <h1 className="page-title">Admin Dog List</h1>
+    </section>
+  );
+};
+
+export default AdminDogList;
