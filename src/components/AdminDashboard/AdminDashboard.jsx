08956e419a27289b04dc88ed29e380cfd5989d8b
Katie K <katiekucsera@gmail.com>
2024-03-14 17:54:30 -0400

    Initiate admin dashboard and basic styling

--- src/components/AdminDashboard/AdminDashboard.jsx
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import "./AdminDashboard.scss";
+import { Link } from "react-router-dom";
 
 function AdminDashboard() {
   return (
-    <div>
-      <h1>Admin Dashboard</h1>
-    </div>
+    <section className="admin-dashboard">
+      <h1 className="page-title">Admin Dashboard</h1>
+      <div className="admin-links">
+        <Link className="admin-link" to="/admin/applications">
+          View Applications
+        </Link>
+        <Link className="admin-link" to="/admin/dogs">
+          View Dogs
+        </Link>
+      </div>
+    </section>
   );
 }
 
