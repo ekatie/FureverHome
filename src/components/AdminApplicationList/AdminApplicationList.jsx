bb3387802eb0c9436410a956c798da1565494bfb
Katie K <katiekucsera@gmail.com>
2024-03-14 17:54:54 -0400

    Initiate admin applications list component

---
+++ src/components/AdminApplicationList/AdminApplicationList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminApplicationList = () => {
+  return (
+    <section className="admin-app-list">
+      <h1 className="page-title">Admin Application List</h1>
+    </section>
+  );
+};
+
+export default AdminApplicationList;
