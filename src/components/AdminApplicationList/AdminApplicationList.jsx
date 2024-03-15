93a4692e91e2b832934b3caf9b74418c89d4acbb
Katie K <katiekucsera@gmail.com>
2024-03-15 15:04:52 -0400

    Implement admin applications list component

--- src/components/AdminApplicationList/AdminApplicationList.jsx
+++ src/components/AdminApplicationList/AdminApplicationList.jsx
@@ -1,9 +1,62 @@
 import React from "react";
+import "../AdminDogList/AdminDogList.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import EditIcon from "@mui/icons-material/Edit";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useNavigate } from "react-router-dom";
+import { fetchAdminApplicationsAsync } from "../../features/applicationSlice";
 
 const AdminApplicationList = () => {
+  const dispatch = useDispatch();
+  const applications = useSelector((state) => state.application.applications);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(fetchAdminApplicationsAsync());
+  }, [dispatch]);
+
   return (
-    <section className="admin-app-list">
-      <h1 className="page-title">Admin Application List</h1>
+    <section className="admin-list">
+      <div className="page-header">
+        <ArrowBackIcon className="back-icon" onClick={() => navigate("/admin/dashboard")} />
+        <h1 className="page-title">Admin Application List</h1>
+      </div>
+      <table className="admin-table">
+        <thead>
+          <tr>
+            <th>Applicant</th>
+            <th>Dog Match</th>
+            <th>Status</th>
+            <th>Created On</th>
+            <th>Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {applications.map((app) => (
+            <tr key={app.id} className="admin-row">
+              <td>{app.user.name}</td>
+              <td>{app.dog.name}</td>
+              <td>{app.status}</td>
+              <td>
+                {new Date(app.created_at).toLocaleDateString("en-US", {
+                  month: "long",
+                  day: "2-digit",
+                  year: "numeric",
+                })}
+              </td>
+              <td>
+                <button
+                  className="edit-button"
+                  onClick={() => navigate(`/admin/applications/${app.id}`)}
+                >
+                  <EditIcon />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </section>
   );
 };
