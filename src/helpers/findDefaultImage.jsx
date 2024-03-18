37d17515b83befe1fc3ae7bd856a02c7aebc400b
Katie K <katiekucsera@gmail.com>
2024-03-18 18:04:58 -0400

    Add commenting

--- src/helpers/findDefaultImage.jsx
+++ src/helpers/findDefaultImage.jsx
@@ -1,3 +1,8 @@
+/**
+ *  This function finds the default image of the dog
+ * @param {*} dog
+ * @returns - the default image of the dog
+ */
 export const findDefaultImage = (dog) => {
   const defaultImage =
     dog.dog_images_attributes.find((image) => image.is_default) ||
