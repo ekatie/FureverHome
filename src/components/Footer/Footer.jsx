b6ef584e58418d06f6d205fd14039b6b848394c5
Katie K <katiekucsera@gmail.com>
2024-03-21 18:40:11 -0400

    Update footer social links to linkedin and github

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,35 +1,32 @@
 import React from "react";
 import "./Footer.scss";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import XIcon from "@mui/icons-material/X";
-import YouTubeIcon from "@mui/icons-material/YouTube";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import BadgeIcon from "@mui/icons-material/Badge";
 
 function Footer() {
   return (
     <footer className="footer">
       <p>Furever Home</p>
       <div className="social-media">
-        <a href="https://www.instagram.com/" className="social-link">
-          <InstagramIcon />
+        <a href="https://github.com/ekatie" className="social-link">
+          <GitHubIcon />
         </a>
-        <a href="https://www.facebook.com/" className="social-link">
-          <FacebookIcon />
+        <a
+          href="https://www.linkedin.com/in/katiekucsera/"
+          className="social-link"
+        >
+          <LinkedInIcon />
         </a>
-        <a href="https://twitter.com" className="social-link">
-          <XIcon />
-        </a>
-        <a href="https://www.youtube.com/" className="social-link">
-          <YouTubeIcon />
+        <a href="https://flowcv.com/resume/3m142pt9t6" className="social-link">
+          <BadgeIcon />
         </a>
       </div>
-      <a href="mailto:info@fureverhome.com" className="email-link">
-        info@fureverhome.com
+      <a href="mailto:katie.kucsera@gmail.com" className="email-link">
+        katie.kucsera@gmail.com
       </a>
     </footer>
   );
 }
 
 export default Footer;
-
-// set up email text to link to send mail to
